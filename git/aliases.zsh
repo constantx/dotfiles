e6dc656251f3a142f2397c6025f3cc74582172af
Truong Nguyen <constantx@gmail.com>
2013-05-02 20:06:41 +0700

    short alias a-z, add git pull --rebase

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -7,13 +7,15 @@ then
 fi
 
 # The rest of my fun git aliases
-alias gl='git pull --prune'
-alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-alias gp='git push origin HEAD'
-alias gd='git diff'
+# alias gl='git pull --prune'
+alias gb='git branch'
 alias gc='git commit'
 alias gca='git commit -a'
 alias gco='git checkout'
-alias gb='git branch'
-alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
+alias gd='git diff'
+alias gl="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias gp='git push origin HEAD'
+alias gpr='git pull --rebase'
+alias grb="git ls-remote --heads origin"
 alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
+alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
