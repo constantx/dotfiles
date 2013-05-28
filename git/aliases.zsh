ad8c34305bdfcda15961710a1d27325a0a1e418b
Truong Nguyen <constantx>
2013-05-28 22:01:35 +0700

    co = checkout alias, comment out simple push since git is complaining

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -19,3 +19,4 @@ alias gpr='git pull --rebase'
 alias grb="git ls-remote --heads origin"
 alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
+alias co="git checkout"
