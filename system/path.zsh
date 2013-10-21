64c0b1dc01bc4ca3cf5e3b8373d9e3bfc2b24c5c
Truong Nguyen <constantx@gmail.com>
2013-10-20 22:11:59 -0700

    update path, add gitflow

--- system/path.zsh
+++ system/path.zsh
@@ -1,7 +1,20 @@
 export PATH=$PATH
 
-export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 
+# Add RVM to PATH for scripting
+export PATH="$PATH:$HOME/.rvm/bin"
+
+export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
+
 export PATH="/usr/local/mongodb/bin:$PATH"
+
+# set redis path
+export PATH="$PATH:/usr/local/redis/src"
+
+### Added by the Heroku Toolbelt
+export PATH="/usr/local/heroku/bin:$PATH"
+
+# set Maestro path
+export PATH="$PATH:$HOME/.maestro/bin"
